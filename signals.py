b9e4fd74012f6825c08db5b5d15d47a08a188ba2
pickman22 <jhestolano@gmail.com>
2016-08-10 22:07:14 -0500

    Cleaned up the code. Now it does not compute the x-axis data anymore. It's computed once in init function and it's used according to the size of y axis data. Auto scale is easy to handle, the thing is that redrawing y-axis and ticks is not as straight forward.

--- signals.py
+++ signals.py
@@ -18,6 +18,9 @@ logging.basicConfig(level = logging.DEBUG)
 logging.getLogger().setLevel(logging.ERROR)
 
 def create_axes(title = None, xlabel = None, ylabel = None, legend = None):
+    '''Create a figure with modified parameters so that it looks more
+    aesthetic. @title is the figure's title, @xlabel is the x-axis' label,
+    @ylabel, the y-axis' label and @legend holds the lines labels.'''
     fig = plt.figure()
     ax = plt.gca()
     if title:
@@ -38,12 +41,42 @@ def create_axes(title = None, xlabel = None, ylabel = None, legend = None):
     ax.yaxis.grid(True, which = 'major')
     ax.xaxis.grid(True, which = 'major')
     return fig, ax
+
+def remove_from_list(data, keep_data):
+    '''Remove elements from list @data so that the length matches @keep_data'''    
+    n = len(data) - keep_data
+    if n <= 0:
+        return data
+    else:
+        logging.debug('Removing %s from list', n)
+        return data[:n]
+
+
+def remove_from_ndarray(data, keep_data):
+    '''Remove elements from @data ndarray so that the length matches @keep_data
+    '''
+    n = data.size - keep_data
+    if n <= 0:
+        return data
+    else:
+        logging.debug('Removing %s from ndarray', n)
+        return np.delete(data, slice(0, n))
   
 class Signal(object):
-
+    '''Defines the interface for Signal. This class specifies how data is
+    stored, deleted and passed on to a RealTimePlot object for visulization'''
     __metaclass__ = ABCMeta
 
-    def __init__(self, names, on_new_data = None, on_connect = None, on_disconnect = None, rate = 10.):
+
+    def __init__(self, names, on_new_data = None, on_connect = None,
+    on_disconnect = None, rate = 10., keep_data = 300):
+        '''@names are the labels on the signals that will be read, either from
+        serial port, sockets or other interface. @on_new_data is a callback that
+        fires every time new data arrives. @on_connect fires when connection
+        is established. @on_discconect fires when the channel closes. @rate
+        specfies the interval at which the channel will be polled for data.
+        @keep_data specifies that amount of data that will be buffered.'''
+
         self.on_new_data = on_new_data
         self.on_connect = on_connect
         self.on_disconnect = on_disconnect   
@@ -51,12 +84,16 @@ class Signal(object):
         self.rate = rate
         self.names = names
         self.is_connected = False
+        self.is_streaming = False
+        self.keep_data = keep_data
 
     def _new_data_callback(self, data):
+        '''Callback that fires when @data is received.'''
         if self.on_new_data:
             self.on_new_data(data)
-            
-    def _stream(self):
+
+    def _stream(self):    
+        '''Called periodically to poll for new data from communication channel.'''            
         raw_data = self._readline()
         try:
             json_data = json.loads(raw_data)
@@ -64,9 +101,14 @@ class Signal(object):
             logging.debug('json: %s', json_data)
         except ValueError:
             logging.error('Could not read data: %s', raw_data)
-        threading.Timer(self.rate / 1000., self._stream).start()
-            
-    def start(self):
+        if self.is_connected:
+            threading.Timer(self.rate / 1000., self._stream).start()
+        else:
+            logging.warn('Not streaming anymore!')
+
+    def start(self):            
+        '''Connect and start receiving data.'''
+        self.connect()
         if self.is_connected:
             self._stream()
             if self.on_connect:
@@ -75,6 +117,7 @@ class Signal(object):
             raise ValueError('Conection must be established first!')
             
     def stop(self):
+        '''Stop and close connection.'''
         self.disconnect()
         if not self.is_connected:
             if self.on_disconnect:
@@ -84,30 +127,44 @@ class Signal(object):
     
     @abstractmethod
     def _readline(self, *params):
+        '''Implement how a data line (string terminated with \n) is read.
+        @params are parameters needed by the channel.'''
         pass
     
     @abstractmethod 
     def connect(self, *params):
+        '''Implements how to connect to the channel. @params are parameters needed
+        for connection.'''
         pass
         
     @abstractmethod
     def disconnect(self, *params):
+        '''Implements how to disconnect from channel. @params are parameters needed
+        to disconnect.'''
         pass
-    
+
     def has_data(self, name):
+        '''Determine if buffer with @name has data stored. Returns the length of
+        the list that hols data if there is. Returns 0 if there is no data. 
+        Returns -1 if there is no buffer that matches @name.'''
         if self.buffer.has_key(name):
             return len(self.buffer[name])
         else:
             return -1
 
-         
     def add_dict_data(self, data_dict):
+        '''Add data to every buffer using @data_dict dictionary.''' 
         for name, data in data_dict.iteritems():
             self.add_data(name, data)
         self._new_data_callback(data_dict)
 
     def add_data(self, name, data):
+        '''Add @data to the buffer with key that matches @name.'''
+        if not self.buffer.has_key(name):
+            return
         self.buffer[name].append(data)
+        if len(self.buffer[name]) > self.keep_data:
+            remove_from_list(self.buffer[name], self.keep_data)
 
     def get_data(self, name, n = 0):
         '''Returns @n amount of data in signal @name. If n == 0, all data in 
@@ -127,7 +184,8 @@ class Signal(object):
         return data
         
     def get_data_dict(self):
-        data = self.buffer
+        '''Returns all data in form of a dictionary.'''
+        data = copy.deepcopy(self.buffer)
         for name in self.buffer.iterkeys():
             del data[name][:]
 
@@ -137,21 +195,29 @@ class SerialSignal(Signal):
     def __init__(self, names, port = '/dev/ttyACM0', baudrate = 9600,
     timeout = 1., rate = 10., on_new_data = None, on_connect = None,
     on_disconnect = None):
+        '''@names specifies the labels of the signals expected from the serial
+        port. @port is the name of the port, @baudrate is the connection's
+        baudrate. @on_new_datam @on_connect and @on_disconnect are callbacks.
+        @timeout specifies the timeout when data reading is attempeted. @rate
+        specifies the interval at which data is polled.'''
         Signal.__init__(self, names, on_new_data, on_connect, on_disconnect)
-        self.connect(port, baudrate, timeout)
+        self.port = serial.Serial(port, baudrate, timeout = timeout)
+        #self.connect(port, baudrate, timeout)
 
     def connect(self, *params):
+        '''Attempt connection with @params.'''
         if self.is_connected:
             return
-        self.port = serial.Serial(params[0], params[1], timeout = params[2])
         if not self.port.is_open:
             self.port.open()
         self.is_connected = self.port.is_open
         
     def _readline(self):
+        '''Read a line of data.'''
         return self.port.readline()
     
     def disconnect(self, *params):
+        '''Attempt disconnection using @params.'''
         if not self.is_connected:
             return
         self.port.close()
@@ -159,10 +225,16 @@ class SerialSignal(Signal):
         
         
 class RealTimePlot(object):
+    '''Defines how data received through a channel (serial port or sockets)
+    is animated.'''
 
     def __init__(self, signal, legend = None, title = None, xlabel = None, ylabel = None,
-    interval = 60, blit = True, xlim = 300., ylim = [-2., 2.],
-    autoscroll = True, autosize = True, keep_data = False):
+    interval = 60, blit = True, xlim = 300., ylim = [-2., 2.], keep_data = 300,
+    autoscroll = True, autosize = True):
+        '''@signal is the object that holds data. @legend for the figure. @title for figure.
+        @xlabel for figure. @ylabel for figure. @interval at which animation is updated.
+        @blit for faster animation. @xlim x-axis limits. @ylim y-axis limits. @keep_data
+        defines how much data is displayed on the figure.'''
         self.fig, self.ax = create_axes()
         if title is not None:
             self.ax.set_title(title)
@@ -172,67 +244,41 @@ class RealTimePlot(object):
             self.ax.set_ylabel(ylabel)
         if legend == None:
             legend = signal.names
-        self.lines = [plt.plot([0.], [0.], label = label)[0] for label in legend]
-        self.xlim = xlim
+        self.lines = [plt.plot([], [], label = label, animated = True)[0] for label in legend]
         self.ax.set_ylim(ylim)
         self.ax.set_xlim([0, xlim])
         self.autoscroll = autoscroll
-        self.autosize = autosize
         self.signal = signal
         self.keep_data = keep_data
         self.animation = animation.FuncAnimation(self.fig, self.update, interval = interval, blit = blit)
+        self.keep_data = keep_data
+        self._xdata = np.arange(keep_data)
         plt.legend()
         plt.show()
-        
-    def _discard_data(self):
-        pass
         
     def _init_plot(self):
         pass
             
     def _handle_autoscroll(self, t):
-        '''If autoscroll is specified, this function handles it. This should
-        not be used by application code.'''
-        if self.autoscroll:
-            # numpy empty arrays have dimension 0!!!!
-            # so we have to use size to take care of this.
-            # Empty arrays have size 0.
-            if t.ndim == 0 and t.size != 0:
-                logging.error('%s', t.ndim)
-                if t > self.xlim:
-                    self.ax.set_xlim([t - self.xlim, t])
-            elif t.ndim > 0:
-                if t[-1] > self.xlim:    
-                    self.ax.set_xlim([t[-1] - self.xlim, t[-1]])
-            else:
-                logging.error('Time vector is unknown type: %s', type(t))
+        pass
                
     def _handle_autosize(self, t):
         pass
-     
+
     def update(self, i):
         '''Gets called to redraw the plot. Should not be used by application
         code.'''
-        idx = 0
         for name, line in zip(self.signal.names, self.lines):
             if self.signal.has_data(name) > 0:
                 _, ly = line.get_data()
                 ly = np.append(ly, self.signal.get_data(name))
-                lx = np.arange(ly.size)
-                line.set_data(lx, ly)
-                idx = max(idx, lx[-1])
+                ly = remove_from_ndarray(ly, self.keep_data)
+                line.set_data(self._xdata[:ly.size], ly)
             else:
-                pass
-        self._handle_autoscroll(idx)
-        #self.fig.canvas.update()
-        #self.ax.figure.canvas.draw()
+                logging.warn('Signal has no data')
         return self.lines
     
 if __name__ == '__main__':
     stream = SerialSignal(['x', 'y', 'z'], baudrate = 115200)
     stream.start()
-    r = RealTimePlot(stream)
-
-
-
-
+    r = RealTimePlot(stream, xlim = 300., keep_data = 300)
