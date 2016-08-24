8beb0b8ec0fcf05e8510a2ee7c37475012ad42a8
pickman22 <jhestolano@gmail.com>
2016-08-23 21:44:59 -0500

    Style changes

--- controller.py
+++ controller.py
@@ -5,40 +5,59 @@ import math
 import logging
 import motor
 
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 logging.getLogger().setLevel(logging.DEBUG)
 
+
 def _compute_gains(K, tau, Ts, Pos):
-    z = abs(math.log(Pos)) / math.sqrt((math.log(Pos)) **2 + math.pi ** 2)
+    z = abs(math.log(Pos)) / math.sqrt((math.log(Pos)) ** 2 + math.pi ** 2)
     wn = 4 / (z * Ts)
     logging.debug('Damping ratio: %s, Natural frequency: %s rads/s', z, wn)
-    k1 = (2 * z * wn * tau) / K # Zero-order coefficient.
-    k2 = (tau * wn ** 2 - 1) / K # First-order coefficient.
+    k1 = (2 * z * wn * tau) / K  # Zero-order coefficient.
+    k2 = (tau * wn ** 2 - 1) / K  # First-order coefficient.
     if -z * wn >= 0.:
         logging.warn('Unstable system!!!')
     logging.debug('Stability term: %s', -z * wn)
     logging.debug('Controller gains k1: %s, k2: %s', k1, k2)
-    return k1, k2    
+    return k1, k2
 
-def _motor_controller(Ts, Pos, K = None, tau = None, R = None,
-    L = None, Kt = None, Kb = None, Kf = None, J = None):
+
+def _motor_controller(Ts, Pos, K=None, tau=None, R=None,
+                      L=None, Kt=None, Kb=None, Kf=None, J=None):
     if K is None or tau is None:
-        if R is None or L is None or Kt is None or Kb is None or Kf is None or J is None:
-            raise ValueError('If K AND tau are not provided, then all motor parameters are needed!')
+        if R is None or L is None or Kt is None or Kb is None or Kf is None or
+        J is None:
+            raise ValueError('If K AND tau are not provided, then all motor \
+                             parameters are needed!')
             return None
         else:
             K, tau = compute_K_and_tau(R, L, J, Kb, Kt, Kf)
     return _compute_gains(K, tau, Ts, Pos)
 
-def motor_velocity_controller(Ts, Pos, K = None, tau = None, R = None, L = None, 
-    Kt = None, Kb = None, Kf = None, J = None):
+
+def velocity_controller(Ts, Pos, K=None, tau=None, R=None, L=None,
+                        J=None, Kt=None, Kb=None, Kf=None):
+    ''' Computes PID velocity controller given motor parameters '''
     kp, ki = _motor_controller(Ts, Pos, K, tau, R, L, Kt, Kb, Kf, J)
-    return PID_Controller(kp = kp, ki = ki, kd = 0.)
-    
-def motor_position_controller(Ts, Pos, K = None, tau = None, R = None, L = None, 
-    Kt = None, Kb = None, Kf = None, J = None, pid = None):
+    return PID_Controller(kp=kp, ki=ki, kd=0.)
+
+
+def position_controller(Ts, Pos, K=None, tau=None, R=None, L=None,
+                        J=None, Kt=None, Kb=None, Kf=None):
+    ''' Computes PID position controller given motor parameters '''
     kd, kp = _motor_controller(Ts, Pos, K, tau, R, L, Kt, Kb, Kf, J)
-    return PID_Controller(kp = kp, kd = kd, ki = 0.)
+    return PID_Controller(kp=kp, kd=kd, ki=0.)
+
+
+def motor_velocity_controller(Ts, Pos, m):
+    ''' Computes PID velocity controller given motor model '''
+    return velocity_controller(Ts, Pos, m.R, m.L, m.J, m.Kt, m.Kb, m.Kf)
+
+
+def motor_position_contrlller(Ts, Pos, m):
+    ''' Computes PID position controller given motor model '''
+    return position_controller(Ts, Pos, m.R, m.L, m.J, m.Kt, m.Kb, m.Kf)
+
 
 def compute_K_and_tau(R, L, J, Kt, Kb, Kf):
     if R <= 10 * L:
@@ -47,10 +66,12 @@ def compute_K_and_tau(R, L, J, Kt, Kb, Kf):
     tau = R * J / (Kt * Kb + R * Kf)
     logging.debug('K: %s, tau: %s', K, tau)
     return K, tau
-    
+
+
 class PID_Controller(object):
 
-    def __init__(self, kp = 0., kd = 0., ki = 0., target = 0., I_MAX = None, I_MIN = None, ts = 20e-3):
+    def __init__(self, kp=0., kd=0., ki=0., target=0., I_MAX=None, I_MIN=None,
+                 ts=20e-3):
         self.kp = kp
         self.kd = kd
         self.ki = ki
@@ -65,15 +86,15 @@ class PID_Controller(object):
 
     def reset(self):
         self._ie = 0.
-        
-    def set_gains(self, kp = None, kd = None, ki = None):
+
+    def set_gains(self, kp=None, kd=None, ki=None):
         if kp is not None:
             self.kp = kp
         if kd is not None:
             self.kd = kd
         if ki is not None:
             self.ki = ki
-        
+
     def _integral(self):
         self._ie += self.error * self.ts
         if self.I_MAX is not None:
@@ -83,71 +104,69 @@ class PID_Controller(object):
             if self._ie < self.I_MIN:
                 self._ie = self.I_MIN
         return self._ie
-       
+
     def _derivative(self):
         de = (self.error - self._preverr) / self.ts
         self._preverr = self.error
         return de
-        
-    def control(self, y, target = None):
+
+    def control(self, y, target=None):
         if target is not None:
             self.target = target
         self.error = self.target - y
-        return self.kp * self.error + self.kd * self._derivative() + self.ki * self._integral()
+        return self.kp * self.error + self.kd * self._derivative() +
+        self.ki * self._integral()
 
-def position_pid(x, t, *args):
-    pid, = args
-    return pid.control(x[0])
-    
-def velocity_pid(x, t, *args):
-    pid, = args
-    return pid.control(x[1])
-    
+
 if __name__ == '__main__':
+
+    def position_pid(x, t, *args):
+        pid, = args
+        return pid.control(x[0])  # Uses position as feedback.
+
+    def velocity_pid(x, t, *args):
+        pid, = args
+        return pid.control(x[1])  # Uses velocity as feedback.
 
     ti = 0.
     tf = 5.
     ts = 0.001
     t = ti
 
-    Ts = 2 # Tiempo de estabilizacion
-    Pos = 0.05 # Sobretiro maximo de 5%.
-    
-    R = 1.
-    Kf = 0.1
-    Kb = 0.01
-    L = 0.05
-    Kt = 0.01
-    J = 0.1
-    
+    Ts = 2  # Settiling time.
+    Pos = 0.05  # Maximum overshoot of 5%.
+
+    R = 1.  # Armature resistance.
+    Kf = 0.1  # Friction coefficient.
+    Kb = 0.01  # Back-emf constant.
+    L = 0.05  # Armature inductance.
+    Kt = 0.01  # Torque constant.
+    J = 0.1  # Rotor's moment of inertia.
+
     m = motor.DCMotor(ts, R, L, J, Kt, Kf, Kb)
-    vel_pid = motor_velocity_controller(Ts, Pos, Kf = Kf, Kb = Kb, L = L, R = R, J = J, Kt = Kt)
-    pos_pid = motor_position_controller(Ts, Pos, Kf = Kf, Kb = Kb, L = L, R = R, J = J, Kt = Kt)
+    vel_pid = velocity_controller(Ts, Pos, Kf=Kf, Kb=Kb, L=L, R=R, J=J, Kt=Kt)
+    pos_pid = position_controller(Ts, Pos, Kf=Kf, Kb=Kb, L=L, R=R, J=J, Kt=Kt)
     pos_pid.ts = ts
     vel_pid.ts = ts
     pos_pid.target = 1.
     vel_pid.target = 1.
-    
-    pt, px, pv = motor.motor_sim(m, ti, tf, ts, pos_pid, on_input = position_pid)
-    vt, vx, vv = motor.motor_sim(m, ti, tf, ts, vel_pid, on_input = velocity_pid)
-    
+
+    pt, px, pv = motor.motor_sim(m, ti, tf, ts, pos_pid, on_input=position_pid)
+    vt, vx, vv = motor.motor_sim(m, ti, tf, ts, vel_pid, on_input=velocity_pid)
+
     fig, ax = plt.subplots(2, 2)
-    
+
     ax[0, 0].plot(pt, px[:, 0:2])
     ax[0, 0].legend(['Position', 'Velocity'])
     ax[0, 0].set_title('Position Controller')
-    ax[1, 0].plot(pt, pv, label = 'Voltage')
-    ax[1, 0].set_title('Control Action')
+    ax[1, 0].plot(pt, pv, label='Voltage')
+    ax[1, 0].set_title('Control Signal')
     ax[1, 0].legend()
-    
-    ax[0, 1].plot(vt, vx[:, 1], label = 'Velocity')
+
+    ax[0, 1].plot(vt, vx[:, 1], label='Velocity')
     ax[0, 1].set_title('Velocity Controller')
     ax[0, 1].legend()
-    ax[1, 1].plot(vt, vv, label = 'Voltage')
-    ax[1, 1].set_title('Control Action')
+    ax[1, 1].plot(vt, vv, label='Voltage')
+    ax[1, 1].set_title('Control Signal')
     ax[1, 1].legend()
     plt.show()
-    
-    
-           
-
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def create_axes(title = None, xlabel = None, ylabel = None, legend = None):
+
+def create_axes(title=None, xlabel=None, ylabel=None, legend=None):
     '''Create a figure with modified parameters so that it looks more
     aesthetic. @title is the figure's title, @xlabel is the x-axis' label,
     @ylabel, the y-axis' label and @legend holds the lines labels.'''
@@ -13,20 +14,24 @@ def create_axes(title = None, xlabel = None, ylabel = None, legend = None):
     if ylabel:
         ax.set_ylabel(ylabel)
     if legend:
-        ax.legend(legend, loc = 'best', frameon = False)
+        ax.legend(legend, loc='best', frameon=False)
     ax.spines['bottom'].set_linewidth(2)
     ax.spines['left'].set_linewidth(2)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    ax.tick_params(labelsize = 14)
-    ax.yaxis.grid(True, which = 'major')
-    ax.xaxis.grid(True, which = 'major')
+    ax.tick_params(labelsize=14)
+    ax.yaxis.grid(True, which='major')
+    ax.xaxis.grid(True, which='major')
     return fig, ax
 
+
 def step(dx, x, ts):
-    if ts <=0.:
+    ''' Euler integration algorithm. @dx is a numpy.ndarray vector that holds
+    the derivative of the system. @x is a numpy.ndarray vector that holds the
+    current state. @ts is the time step. '''
+    if ts <= 0.:
         raise ValueError('Timestep must be positive')
         return np.zeros(x.shape)
     try:
